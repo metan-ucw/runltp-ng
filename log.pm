681ebdf5ba49f4cf30386e1678601414ba8a7281
Li Wang <liwang@redhat.com>
2021-05-23 17:44:49 +0800

    runltp-ng: use SPDX-License-Identifier
    Signed-off-by: Li Wang <liwang@redhat.com>

--- log.pm
+++ log.pm
@@ -1,22 +1,8 @@
 #!/usr/bin/env perl
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2017-2021 Cyril Hrubis <chrubis@suse.cz>
 #
 # Linux Test Project test runner
-#
-# Copyright (c) 2017-2018 Cyril Hrubis <chrubis@suse.cz>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 use strict;
 use warnings;
