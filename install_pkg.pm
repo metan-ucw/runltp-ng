metan-ucw/runltp-ng

commit 4094e2feddbce0817396befba091b9c11af21407

install_pkg: Add Alpine
Closes: #30

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

install_pkg.pm
@@ -42,6 +42,12 @@ sub foo_to_pkg
 		'kernel-devel-debian' => 'linux-headers-`uname -r`',
 
 		# devel libs
+		'libaio-devel-alpine' => 'libaio-devel',
+		'libacl-devel-alpine' => 'libacl-devel',
+		'libattr-devel-alpine' => 'libattr-devel',
+		'libcap-devel-alpine' => 'libcap-devel',
+		'libnuma-devel-alpine' => 'numactl-devel',
+
 		'libaio-devel-debian' => 'libaio-dev',
 		'libacl-devel-debian' => 'libacl1-dev',
 		'libattr-devel-debian' => 'libattr1-dev',
@@ -71,14 +77,10 @@ sub detect_distro
 {
 	my ($self) = @_;
 
-	if (utils::run_cmd_retry($self, 'grep -q debian /etc/os-release') == 0) {
-		return "debian";
-	}
-	if (utils::run_cmd_retry($self, 'grep -q fedora /etc/os-release') == 0) {
-		return "fedora";
-	}
-	if (utils::run_cmd_retry($self, 'grep -q suse /etc/os-release') == 0) {
-		return "suse";
+	for my $distro (qw(alpine debian fedora suse)) {
+		if (utils::run_cmd_retry($self, "grep -q $distro /etc/os-release") == 0) {
+			return $distro;
+		}
 	}
 
 	print("Unknown distribution!\n");

commit 5db77d527a9dd85dec0f78a006d53b36afb85e26

install_pkg: Fix alpine package names
Fixes: 4094e2f ("install_pkg: Add Alpine")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

install_pkg.pm
@@ -28,11 +28,13 @@ sub foo_to_pkg
 		'xfsprogs' => 'xfsprogs',
 		'e2fsprogs' => 'e2fsprogs',
 		'btrfsprogs' => 'btrfsprogs',
+		'btrfsprogs-alpine' => 'btrfs-progs',
 		'btrfsprogs-debian' => 'btrfs-progs',
 		'btrfsprogs-fedora' => 'btrfs-progs',
 
 		# FS quota tools
 		'quota' => 'quota',
+		'quota-alpine' => 'quota-tools',
 
 		# NFS tools
 		'nfs-utils' => 'nfs-utils',
@@ -40,14 +42,16 @@ sub foo_to_pkg
 
 		# kernel devel
 		'kernel-devel' => 'kernel-devel',
+		'kernel-devel-alpine' => 'linux-headers',
 		'kernel-devel-debian' => 'linux-headers-`uname -r`',
 
 		# devel libs
-		'libaio-devel-alpine' => 'libaio-devel',
-		'libacl-devel-alpine' => 'libacl-devel',
-		'libattr-devel-alpine' => 'libattr-devel',
-		'libcap-devel-alpine' => 'libcap-devel',
-		'libnuma-devel-alpine' => 'numactl-devel',
+		'libacl-devel-alpine' => 'acl-dev',
+		'libaio-devel-alpine' => 'libaio-dev',
+		'libattr-devel-alpine' => 'attr-dev',
+		'libcap-devel-alpine' => 'libcap-dev',
+		'libnuma-devel-alpine' => 'numactl-dev',
+		'pkg-config-alpine' => 'pkgconf',
 
 		'libaio-devel-debian' => 'libaio-dev',
 		'libacl-devel-debian' => 'libacl1-dev',