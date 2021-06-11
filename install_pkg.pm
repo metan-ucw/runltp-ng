metan-ucw/runltp-ng

commit 4094e2feddbce0817396befba091b9c11af21407

install_pkg: Add Alpine
Closes: #30

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

install_pkg.pm
@@ -42,6 +42,12 @@ sub foo_to_pkg
 		'kernel-devel-debian' => 'linux-headers-`uname -r`',
 
 		# devel libs
+		'libaio-devel-alpine' => 'libaio-devel',
+		'libacl-devel-alpine' => 'libacl-devel',
+		'libattr-devel-alpine' => 'libattr-devel',
+		'libcap-devel-alpine' => 'libcap-devel',
+		'libnuma-devel-alpine' => 'numactl-devel',
+
 		'libaio-devel-debian' => 'libaio-dev',
 		'libacl-devel-debian' => 'libacl1-dev',
 		'libattr-devel-debian' => 'libattr1-dev',
@@ -71,14 +77,10 @@ sub detect_distro
 {
 	my ($self) = @_;
 
-	if (utils::run_cmd_retry($self, 'grep -q debian /etc/os-release') == 0) {
-		return "debian";
-	}
-	if (utils::run_cmd_retry($self, 'grep -q fedora /etc/os-release') == 0) {
-		return "fedora";
-	}
-	if (utils::run_cmd_retry($self, 'grep -q suse /etc/os-release') == 0) {
-		return "suse";
+	for my $distro (qw(alpine debian fedora suse)) {
+		if (utils::run_cmd_retry($self, "grep -q $distro /etc/os-release") == 0) {
+			return $distro;
+		}
 	}
 
 	print("Unknown distribution!\n");

commit d9de4135c60bfb3f83daf88c5b43f887e8c1ea43

install_pkg: Add missing Alpine apk support
Also unify style in install_pkg() with other functions.

Fixes: 4094e2f ("install_pkg: Add Alpine")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

install_pkg.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (c) 2017-2021 Cyril Hrubis <chrubis@suse.cz>
+# Copyright (c) 2021 Petr Vorel <pvorel@suse.cz>
 #
 # Linux Test Project test runner
 
@@ -130,11 +131,19 @@ sub install_pkg
 {
 	my ($distro, $pkgs) = @_;
 
+	if ($distro eq "alpine") {
+		return 'apk add ' . join(' ', @$pkgs);
+	}
+
 	if ($distro eq 'debian') {
 		return 'apt-get install -y ' . join(' ', @$pkgs);
-	} elsif ($distro eq 'fedora') {
+	}
+
+	if ($distro eq 'fedora') {
 		return 'yum install -y ' . join(' ', @$pkgs);
-	} elsif ($distro eq 'suse') {
+	}
+
+	if ($distro eq 'suse') {
 		return 'zypper --non-interactive --ignore-unknown in ' . join(' ', @$pkgs);
 	}
 }
@@ -142,6 +151,10 @@ sub install_pkg
 sub update_pkg_db
 {
 	my ($distro) = @_;
+
+	if ($distro eq "alpine") {
+		return "apk update";
+	}
 
 	if ($distro eq "debian") {
 		return "apt-get update";