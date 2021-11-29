metan-ucw/runltp-ng

commit 22db0cd1976f6e2cb80ce6113c89a71d18e2f546

install_pkg: Introduce openSUSE, SLES instead of suse
Although packages are the same (thus map sles to opensuse in package
list in foo_to_pkg()).

Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

install_pkg.pm
@@ -13,6 +13,7 @@ use warnings;
 sub foo_to_pkg
 {
 	my ($foo, $distro) = @_;
+
 	my %pkg_map = (
 		'git' => 'git',
 		'unzip' => 'unzip',
@@ -65,12 +66,16 @@ sub foo_to_pkg
 		'libcap-devel-fedora' => 'libcap-devel',
 		'libnuma-devel-fedora' => 'numactl-devel',
 
-		'libaio-devel-suse' => 'libaio-devel',
-		'libacl-devel-suse' => 'libacl-devel',
-		'libattr-devel-suse' => 'libattr-devel',
-		'libcap-devel-suse' => 'libcap-devel',
-		'libnuma-devel-suse' => 'libnuma-devel',
+		'libaio-devel-opensuse' => 'libaio-devel',
+		'libacl-devel-opensuse' => 'libacl-devel',
+		'libattr-devel-opensuse' => 'libattr-devel',
+		'libcap-devel-opensuse' => 'libcap-devel',
+		'libnuma-devel-opensuse' => 'libnuma-devel',
 	);
+
+	if ($distro eq 'sles') {
+		$distro = 'opensuse';
+	}
 
 	my $pkg = $pkg_map{"$foo-$distro"};
 	return $pkg if defined $pkg;
@@ -78,7 +83,7 @@ sub foo_to_pkg
 	return $pkg_map{"$foo"};
 }
 
-my @distros = qw(alpine debian fedora suse);
+my @distros = qw(alpine debian fedora opensuse sles);
 
 sub detect_distro
 {

commit e858293d8ae855a53cf2a2948be86885e6fe68ca

install_pkg: Fix --cmd for sles, opensuse, ubuntu
These commands prints nothing:
./install_pkg.pm --build --run --distro sles --cmd
./install_pkg.pm --build --run --distro opensuse --cmd
./install_pkg.pm --build --run --distro ubuntu --cmd

Fixes: 22db0cd ("install_pkg: Introduce openSUSE, SLES instead of suse")
Fixes: e572aa5 ("install_pkg: Introduce Ubuntu")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

install_pkg.pm
@@ -121,6 +121,7 @@ sub detect_distro
 sub pkg_to_m32
 {
 	my ($distro, $pkg_name) = @_;
+	$distro = get_distro_alias($distro);
 
 	if ($distro eq "debian") {
 		#TODO: we need architecture detection for now default to i386
@@ -128,7 +129,7 @@ sub pkg_to_m32
 		return "$pkg_name:i386";
 	}
 
-	return "$pkg_name-32bit" if ($distro eq "suse");
+	return "$pkg_name-32bit" if ($distro eq "opensuse");
 
 	return;
 }
@@ -136,10 +137,12 @@ sub pkg_to_m32
 sub setup_m32
 {
 	my ($distro) = @_;
+	$distro = get_distro_alias($distro);
 
 	if ($distro eq "debian") {
 		return ("dpkg --add-architecture i386");
 	}
+
 	return;
 }
 
@@ -159,6 +162,7 @@ sub map_pkgs
 sub install_pkg
 {
 	my ($distro, $pkgs) = @_;
+	$distro = get_distro_alias($distro);
 
 	if ($distro eq "alpine") {
 		return 'apk add ' . join(' ', @$pkgs);
@@ -172,7 +176,7 @@ sub install_pkg
 		return 'yum install -y ' . join(' ', @$pkgs);
 	}
 
-	if ($distro eq 'suse') {
+	if ($distro eq 'opensuse') {
 		return 'zypper --non-interactive --ignore-unknown in ' . join(' ', @$pkgs);
 	}
 }
@@ -180,6 +184,7 @@ sub install_pkg
 sub update_pkg_db
 {
 	my ($distro) = @_;
+	$distro = get_distro_alias($distro);
 
 	if ($distro eq "alpine") {
 		return "apk update";
@@ -193,7 +198,7 @@ sub update_pkg_db
 		return "yum update -y";
 	}
 
-	if ($distro eq "suse") {
+	if ($distro eq "opensuse") {
 		return "zypper --non-interactive ref";
 	}
 