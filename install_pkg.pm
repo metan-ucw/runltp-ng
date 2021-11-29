metan-ucw/runltp-ng

commit e572aa53554e66d77fe07a7169ecada571e79cf9

install_pkg: Introduce Ubuntu
as alias to Debian.

Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

install_pkg.pm
@@ -77,13 +77,17 @@ sub foo_to_pkg
 		$distro = 'opensuse';
 	}
 
+	if ($distro eq 'ubuntu') {
+		$distro = 'debian';
+	}
+
 	my $pkg = $pkg_map{"$foo-$distro"};
 	return $pkg if defined $pkg;
 
 	return $pkg_map{"$foo"};
 }
 
-my @distros = qw(alpine debian fedora opensuse sles);
+my @distros = qw(alpine debian fedora opensuse sles ubuntu);
 
 sub detect_distro
 {

commit 43194665c72cd5a0a38d35e8eef37992ca42de57

install_pkg: Fix distro aliasing
Problem affect SLES and Ubuntu, but visible only on Ubuntu.

Fixes: e572aa5 ("install_pkg: Introduce Ubuntu")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

install_pkg.pm
@@ -76,6 +76,21 @@ sub foo_to_pkg
 		'libnuma-devel-opensuse' => 'libnuma-devel',
 	);
 
+	my $pkg;
+
+	$pkg = $pkg_map{"$foo-$distro"};
+	return $pkg if defined $pkg;
+
+	$pkg = $pkg_map{"$foo-" . get_distro_alias($distro)};
+	return $pkg if defined $pkg;
+
+	return $pkg_map{"$foo"};
+}
+
+sub get_distro_alias
+{
+	my ($distro) = @_;
+
 	if ($distro eq 'sles') {
 		$distro = 'opensuse';
 	}
@@ -84,10 +99,7 @@ sub foo_to_pkg
 		$distro = 'debian';
 	}
 
-	my $pkg = $pkg_map{"$foo-$distro"};
-	return $pkg if defined $pkg;
-
-	return $pkg_map{"$foo"};
+	return $distro;
 }
 
 my @distros = qw(alpine debian fedora opensuse sles ubuntu);